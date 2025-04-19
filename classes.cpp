1d77c0e7dc0bbe60ecf2593d4053b203a900b94e
DornishApril <samireformed786@gmail.com>
2025-04-19 14:22:35 +0600

    classes added, needs to be defined

--- classes.cpp
+++ classes.cpp
@@ -6,7 +6,7 @@
 #include<math.h>
 
 
-//
+// 
 class window {
 };
 
@@ -15,3 +15,14 @@ class scrollbar{
 
 
 
+class windows_window{
+};
+
+class windows_scrollbar{
+};  
+
+class linux_window{
+};  
+
+class linux_scrollbar{
+};  
